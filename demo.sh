4670c9f4a9e940bbf3bf15c661a34a5c18497274
chengzi07 <yihanchen7@163.com>
2018-05-09 23:46:02 +0800

    改动demo

--- demo.sh
+++ demo.sh
@@ -1,13 +1,19 @@
-mkdir $1
-cd $1
-mkdir css js
-echo "<!DOCTYPE>
- <title>Hello</title>
- <h1>Hi</h1>">index.html
-cd css
-echo "h1{color: red;}">style.css
-cd ..
-cd js
-echo "var string = "Hello World"
- alert(string)">main.js
-exit
+if[ -d $1 ];then
+  echo'error:dir exist'
+  exit
+else
+  mkdir $1
+  cd $1
+  mkdir css js
+  echo "<!DOCTYPE>
+<title>Hello</title>
+<h1>Hi</h1>">index.html
+  cd css
+  echo "h1{color: red;}">style.css
+  cd ..
+  cd js
+  echo "var string = "Hello World"
+alert(string)">main.js
+  echo'success'
+  exit
+fi
